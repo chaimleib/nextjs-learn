55e7b01a43a7022ec330d9b80f15c9650b6c2b18
Chaim Halbert <chaim.leib.halbert@gmail.com>
2023-04-21 05:42:42 -0700

    practice Head, Image, Link tags

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function() {
-  return <h1>First Post</h1>;
+  return <>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <h1>First Post</h1>
+    <Image src="/images/profile.jpg" height={144} width={144} alt="profile" />
+    <h2>
+      <Link href="/">Go back home</Link>
+    </h2>
+  </>;
 }
