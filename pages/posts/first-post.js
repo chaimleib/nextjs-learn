79703cc3d90438b8ea6b6381945ff8e38c713f23
Chaim Halbert <chaim.leib.halbert@gmail.com>
2023-04-21 05:50:14 -0700

    component css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,19 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function() {
-  return <>
-    <Head>
-      <title>First Post</title>
-    </Head>
-    <h1>First Post</h1>
-    <Image src="/images/profile.jpg" height={144} width={144} alt="profile" />
-    <h2>
-      <Link href="/">Go back home</Link>
-    </h2>
-  </>;
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Image src="/images/profile.jpg" height={144} width={144} alt="profile" />
+      <h2>
+        <Link href="/">Go back home</Link>
+      </h2>
+    </Layout>
+  );
 }
